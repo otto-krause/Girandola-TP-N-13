0066be71e5fa5285033cc21a5b8d71ac4234d0b5
Girandola Santiago <87081164+GirandolaSantiago@users.noreply.github.com>
2021-11-02 11:02:44 -0300

    Add files via upload

---
+++ ej7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+	{
+
+int a[15];
+	for(int i = 0; i < 15; i++)
+		
+		{
+		
+		printf("Ingrese la nota correspondiente del alumno %d", i);
+			scanf("%d", a[i]);
+			
+		}
+	
+	for(int i = 0; i < 15; i++)
+		
+			{
+		
+	if(a[i] > 8)
+		printf("El alumno %d tiene mas de 8\n", a[i]);
+	
+			}
+	
+		return 0;
+	
+	}
+	
+	
